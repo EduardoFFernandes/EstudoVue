3a25afcb4389c0a1cb15abacbf8f4e9c19bd9b22
EduardoFFernandes <43785969+EduardoFFernandes@users.noreply.github.com>
2021-02-23 11:14:06 -0300

    Primeiro commit

---
+++ src/components/Cliente.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="cliente">
+        <h1>Componente Cliente</h1>
+        <p>{{ nome }}</p>
+        <p>{{ descricao }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            nome: "Eduardo Freire Fernandes",
+            numero: "27",
+            email: "email@email",
+            idade: 23,
+            descricao:"Lorem ipsum it Lorem"
+        };
+    },
+};
+</script>
+
+<style scoped>
+.cliente {
+    color: darkblue;
+    text-align: center;
+}
+</style>
