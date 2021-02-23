1cd6b1eb08d40d16c05918d112cd172013eac36f
EduardoFFernandes <43785969+EduardoFFernandes@users.noreply.github.com>
2021-02-23 14:50:30 -0300

    Eventos e Bind de Classes

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,68 @@
 <template>
-  <div id="app">
-    <Cliente/>
-  </div>
-
+    <div id="app">
+        <h1>Clientes</h1>
+        <div class="clientes">
+          <div v-for="cliente in clientes" :key="cliente.id">
+              <Cliente :cliente="cliente" />
+          </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import Cliente from "./components/Cliente"
+import Cliente from "./components/Cliente";
 export default {
-  name: 'App',
-  components: {
-    Cliente
-  }
-}
+    name: "App",
+    data() {
+        return {
+            clienteEduardo: {
+                nome: "Eduardo Freire Fernandes",
+                descricao: "Descricao do objeto",
+            },
+            clientes: [
+                {
+                    id: 1,
+                    nome: "Eduardo Freire Fernandes",
+                    descricao: "Descricao 1",
+                },
+                {
+                    id: 2,
+                    nome: "Fernando Freire Fernandes",
+                    descricao: "Descricao 2",
+                },
+                {
+                    id: 3,
+                    nome: "Joao Freire Fernandes",
+                    descricao: "Descricao 3",
+                },
+                {
+                    id: 4,
+                    nome: "Gabriel Freire Fernandes",
+                    descricao: "Descricao 4",
+                },
+                {
+                    id: 5,
+                    nome: "Allison Freire Fernandes",
+                    descricao: "Descricao 5",
+                },
+            ],
+        };
+    },
+    components: {
+        Cliente,
+    },
+};
 </script>
 
 <style scoped>
-
+  #app {
+    text-align: center;
+  }
+  .clientes {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    margin: 0px 10% 0px 10%;
+  }
 </style>
--- src/components/Cliente.vue
+++ src/components/Cliente.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="cliente">
-        <input type="text" v-model="nome">
+    <div class="cliente" :class="{ 'cliente-normal': !isPremium, 'cliente-premium': isPremium }">
+        <!-- <input type="text" v-model="cliente.nome">
         <hr>
-        <input type="text" v-model="descricao">
-        <h1>Componente Cliente</h1>
-        <p>Nome: {{ nome }}</p>
-        <p>Descrição: {{ descricao }}</p>
+        <input type="text" v-model="cliente.descricao"> -->
+        <p>Nome: {{ cliente.nome }}</p>
+        <p style="width: 100%">Descrição: {{ cliente.descricao }}</p>
+        <button @click="mudarCor()">Mudar a Cor</button>
     </div>
 </template>
 
@@ -13,19 +13,36 @@
 export default {
     data() {
         return {
-            nome: "Eduardo Freire Fernandes",
-            numero: "27",
-            email: "email@email",
-            idade: 23,
-            descricao:"Lorem ipsum it Lorem"
+            isPremium: false,
         };
+    },
+    props: {
+        cliente: Object,
+    },
+    methods: {
+        mudarCor: function () {
+            this.isPremium = !this.isPremium;
+            console.log("Click");
+        },
     },
 };
 </script>
 
 <style scoped>
 .cliente {
-    color: darkblue;
+    width: 250px;
+    height: 200px;
+    display: flex;
+    justify-content: center;
     text-align: center;
+    flex-wrap: wrap;
+    color: darkblue;
+    flex-grow: 4;
+}
+.cliente-normal {
+    background-color: aquamarine;
+}
+.cliente-premium {
+    background-color: gold;
 }
 </style>
